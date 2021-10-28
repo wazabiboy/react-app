6dc6db202a305e71d3ac1c2b49a083bb2fcb6e6c
wazabi.boy <wazabi.boy@gmail.com>
2021-10-28 16:56:14 +0200

    romaina

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -38,8 +38,8 @@ class Counter extends Component {
             <>
 
                 <span>{this.state.count}</span> <br /><br />
-                <button type="button" className="btn btn-" onClick={() => this.handleChange("plus")}> + </button>
-                ||  <button type="button" className="btn btn-primary" onClick={() => this.handleChange("moins")}> - </button>
+                <button type="button" className="btn btn-primary" onClick={() => this.handleChange("plus")}> + </button>
+                ||  <button type="button" className="btn btn-" onClick={() => this.handleChange("moins")}> - </button>
                 ||  <button type="button" className="btn btn-danger" onClick={() => this.handleChange("zero")}> Reset</button>
 
                 <br /><br />
